24c279f4625f5cdc1de99ab980bcecc8af714a7b
Your Name <you@example.com>
2023-07-15 01:30:51 -0300

    Criação da Estrutura basica

---
+++ Projeto-integrador/src/main/java/PJP2023/Projeto/integrador/ProjetoIntegradorApplication.java
@@ -0,0 +1,15 @@
+package PJP2023.Projeto.integrador;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorApplication {
+
+	public static void main(String[] args) {
+
+		Application.launch(JavaFxApplication.class, args);
+	}
+
+}
